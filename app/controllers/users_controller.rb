564c0fc025537bc594c63f5ca3bc1ace034fa868
lequangcanh <lequangcanh94@gmail.com>
2016-12-08 20:37:08 +0700

    edit-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: :edit
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -14,9 +16,23 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def update
+    if @user.update_attributes name: params[:user][:name]
+      flash[:success] = t ".success_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    render file: Settings.page_404_url unless current_user.is_user? @user
   end
 end
